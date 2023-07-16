11eeeb0abb72f5e38b5834c1ceac0dbd37958f9e
codeauthor1 <codeauthor2000@gmail.com>
2023-07-16 17:16:36 +0100

    chore: update package.json

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'lib/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'esm',
+    },
+  ],
+  plugins: [typescript()],
+};
